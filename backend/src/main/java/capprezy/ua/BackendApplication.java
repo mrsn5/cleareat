e549e07866d509197d7f9af933343829a88310ea
San Nguyen <mr.sn5.kma@gmail.com>
2020-02-06 15:25:34 +0200

    hot fix

--- backend/src/main/java/capprezy/ua/BackendApplication.java
+++ backend/src/main/java/capprezy/ua/BackendApplication.java
@@ -2,8 +2,6 @@ package capprezy.ua;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 public class BackendApplication {

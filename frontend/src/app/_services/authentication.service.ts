3869531812cdb29c4e184dec16348ab2d2798e27
c-tel <stas.telizhenko@gmail.com>
2020-04-21 21:55:42 +0300

    make currentUser replay

--- frontend/src/app/_services/authentication.service.ts
+++ frontend/src/app/_services/authentication.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import {User} from "../_models/user";
 import { ApiClientService } from './api-client.service';
@@ -7,11 +7,13 @@ import { ApiClientService } from './api-client.service';
 @Injectable({ providedIn: 'root' })
 export class AuthenticationService {
   private currentUserSubject: BehaviorSubject<User>;
+  private currentUserReplay: ReplaySubject<User> = new ReplaySubject<User>(1);
   public currentUser: Observable<User>;
 
   constructor(private apiClient: ApiClientService) {
     this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
-    this.currentUser = this.currentUserSubject.asObservable();
+    this.currentUser = this.currentUserReplay.asObservable();
+    this.currentUserSubject.subscribe(this.currentUserReplay);
   }
 
   public get currentUserValue(): User {
